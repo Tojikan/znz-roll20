95cab9921268ddeabdd09510825981f4f23caee7
Tojikan <andrew.eqian@gmail.com>
2020-08-19 02:26:40 -0400

    added roll buttons, tweaks to attack script, styling for hidden roll buttons, moved flashlight

--- sheet/script-attack.js
+++ sheet/script-attack.js
@@ -1,6 +1,6 @@
 var Attack = Attack || (function() {
     'use strict';
-    const  fields = {"weaponFields":{"meleetype":"melee_type","durability":"melee_durability","meleecost":"melee_energy_cost","meleedamage":"melee_damage","meleecrit":"melee_crit_bonus","rangedtype":"ranged_type","ammo":"ranged_ammo","reloads":"ranged_reloads","rangeddamage":"ranged_damage","rangedcrit":"weapon_crit_bonus","range":"weapon_range"},"combatProfs":["prof_handguns","prof_shotguns","prof_rifles","prof_projectile","prof_throwing","prof_sharp_melee","prof_blunt_melee","prof_heavy_melee"]},
+    const  fields = {"weaponFields":{"meleetype":"melee_type","durability":"melee_durability","meleecost":"melee_energy_cost","meleedamage":"melee_damage","meleecrit":"melee_crit_bonus","reach":"weapon_reach","rangedtype":"ranged_type","ammo":"ranged_ammo","reloads":"ranged_reloads","rangeddamage":"ranged_damage","rangedcrit":"weapon_crit_bonus","range":"weapon_range"},"combatProfs":["prof_handguns","prof_shotguns","prof_rifles","prof_projectile","prof_throwing","prof_sharp_melee","prof_blunt_melee","prof_heavy_melee"]},
         strengthAttr = "strength",
         dextAttr = "dexterity",
         energyStat = "energy",
@@ -335,13 +335,33 @@ var Attack = Attack || (function() {
             attrType = isMelee ? 'Strength' : 'Dexterity',
             totalAttackBonus = attack.profBonus + attack.attrBonus + attack.hitBonus;
 
-        log(type);
+        var difficulty = ""
+        switch(attack.baseDifficulty) {
+            case 0:
+                difficulty = "Easy";
+                break;
+            case 1:
+                difficulty = "Medium";
+                break;
+            case 2:
+                difficulty = "Hard";
+                break;
+            case 3:
+                difficulty = "Insane";
+                break;
+            case 4:
+                difficulty = "Impossible";
+                break;
+            default:
+                difficulty = "Medium (Unk)"
 
+        }
         
         var outputText = `\
         <h4>${typeText} Attack</h4>\
         <div>${name} tries to attack ${attack.numAttacks} time(s) with ${weaponName.length > 0 ? `their ${weaponName}` : 'their weapon'}</div><br/>\
         ${attack.reversed ? (isMelee ? `<div>${name} melee attacks with their ranged weapon!</div><br/>`: `<div>${name} throws their melee weapon!</div><br/>`) : '' }\
+        <div>Difficulty: ${difficulty}</div><br/>\
         <div>Crit Multiplier: <strong>${attack.critBonus}x</strong></div>\
         <div style="margin-bottom: 10px;">${attrType} Bonus: <strong>${attack.attrBonus}</strong>  |  Proficiency Bonus: <strong>${attack.profBonus}</strong>  |  Misc Bonus: <strong>${attack.hitBonus}</strong>  |  Total Bonus: <strong>${totalAttackBonus}</strong></div>`;
 
